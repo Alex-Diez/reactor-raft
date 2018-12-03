c9e67f339d68eb795c50670a71c5ac99a4798a47
Alex Dukhno <Alex-Diez@users.noreply.github.com>
2018-12-03 19:48:46 +0200

    added code coverage check

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Mission impossible: The endeavour of [raft consensus](https://raft.github.io/raft.pdf) algorithm implementation with [project reactor](https://projectreactor.io)
 
 [![Build Status](https://travis-ci.com/Alex-Diez/reactor-raft.svg?branch=master)](https://travis-ci.com/Alex-Diez/reactor-raft)
+[![Coverage Status](https://coveralls.io/repos/github/Alex-Diez/reactor-raft/badge.svg)](https://coveralls.io/github/Alex-Diez/reactor-raft)
--- src/main/java/org/algorithms/Event.java
+++ src/main/java/org/algorithms/Event.java
@@ -1,6 +1,5 @@
 package org.algorithms;
 
-import java.util.Objects;
 import java.util.UUID;
 
 class Event {
@@ -12,31 +11,5 @@ class Event {
     this.sourceId = sourceId;
     this.targetId = targetId;
     this.type = type;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o != null && getClass().equals(o.getClass())) {
-      Event event = (Event) o;
-      return type == event.type &&
-          Objects.equals(sourceId, event.sourceId) &&
-          Objects.equals(targetId, event.targetId);
-    }
-    return false;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(sourceId, type);
-  }
-
-  @Override
-  public String toString() {
-    return "Event{" +
-        "sourceId=" + sourceId +
-        ", targetId=" + targetId +
-        ", type=" + type +
-        '}';
   }
 }
